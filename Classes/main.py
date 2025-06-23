eca716eb4bad0c082fce7b1954e86ba111f6a844
xandealm <amedradoandrade@gmail.com>
2025-06-23 18:09:59 -0300

    Update main.py

--- Classes/main.py
+++ Classes/main.py
@@ -1,9 +1,6 @@
 # main.py
-from dados import Dados
-from dono import Dono 
-
-# Como a classe Dados é abstrata, não podemos criar uma instância dela.
-# Em vez disso, criamos uma instância de uma de suas classes filhas, como Dono,
+from dados import Dados # Como a classe Dados é abstrata, não podemos criar uma instância dela.
+from dono import Dono # Em vez disso, criamos uma instância de uma de suas classes filhas, como Dono,
 # para iniciar o processo. A tela de login é a mesma para todos.
 
 if __name__ == "__main__":
